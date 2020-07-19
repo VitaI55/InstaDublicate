01128e071a4f8b033214886bb2b3b1622d74a1f9
Kurama <59122866+VitaI55@users.noreply.github.com>
2020-07-20 00:11:03 +0300

    V 1.0

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/jgSRfM4f6DJnuP1hrUES6Dne0LadKT2WGfiKAcEs.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-mail.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to My House
+
+This is my first Laravel project, hope you'll enjoy it
+
+
+
+All the best,<br>
+Vitalii
+@endcomponent
